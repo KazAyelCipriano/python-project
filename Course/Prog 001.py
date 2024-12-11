72db8aa2e752e796f25dfe4ce3a8690c072d0930
Cipriano <professorpedrocipriano@gmail.com>
2024-12-11 18:32:33 -0300

    Add files via upload

---
+++ Course/Prog 001.py
@@ -0,0 +1,34 @@
+Python 3.13.1 (tags/v3.13.1:0671451, Dec  3 2024, 19:06:28) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+>>> nome = "Josue"
+>>> sobrenome = "Pedro"
+>>> idade = 28
+>>> pais = "Brasil"
+>>> print(nome)
+Josue
+>>> print(type(nome))
+<class 'str'>
+>>> print(type(idade))
+<class 'int'>
+>>> idade = 28.2
+>>> print(type(idade))
+<class 'float'>
+>>> print("Meu nome é "+nome+" "+sobrenome)
+Meu nome é Josue Pedro
+>>> print("Tenho " + str(idade) +" anos ")
+Tenho 28.2 anos 
+>>> idade=str(idade)
+>>> print("Tenho " + idade)
+Tenho 28.2
+>>> numweo_1 = 20
+>>> numweo_2 = 30
+>>> resultado = numweo_1 * numweo_2
+>>> print(resultado)
+600
+>>> numero_! = str(input("Digite um numero: "))
+SyntaxError: invalid syntax
+>>> numero_1 = str(input("Digite um numero: "))
+Digite um numero: 58
+>>> print(numero_1)
+58
---
+++ Course/Prog 002.py
@@ -0,0 +1,25 @@
+Python 3.13.1 (tags/v3.13.1:0671451, Dec  3 2024, 19:06:28) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> nome = "Pedro Cipriano"
+>>> print(nome)
+Pedro Cipriano
+>>> print(input("Qual é o seu nome? "))
+Qual é o seu nome? Pedro
+Pedro
+>>> print(nome)
+Pedro Cipriano
+>>> nome = input("Qual é o seu nome? ")
+Qual é o seu nome? Pedro
+>>> print(nome)
+Pedro
+>>> print(f"Meu nome é {nome}")
+Meu nome é Pedro
+>>> nome = "Pedro"
+>>> sobrenome = "Cipriano"
+>>> idade = 32
+>>> idade = str(idade)
+>>> pais = "Brasil"
+>>> print(f"Meu nome é {nome} {sobrenome} \nTenho {idade} anos \n Moro no {pais}")
+Meu nome é Pedro Cipriano 
+Tenho 32 anos 
+ Moro no Brasil
